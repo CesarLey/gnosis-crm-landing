c12223d9a8ddfb10892d6ee5fd8543a0c0fc9f00
AndrewStevenDJ <andrewstevendiazj@gmail.com>
2026-02-16 16:21:32 -0600

    .

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
     apiUrl: 'https://gnosis-api.onrender.com', // URL del Backend
-    crmUrl: 'https://gnosis-crm-frontend.vercel.app', // URL del CRM Prod
+    crmUrl: 'https://gnosis-crm-frontend-c395.vercel.app', // URL del CRM Prod
     paypalClientId: 'AcWuv4-mo0o9ST-fQyykx-UawZyI3HVm5rs9PidjIlYcFER61a5Lgz758dHA2A_F6N_-Te9NB4cDQ1mC'
 };
