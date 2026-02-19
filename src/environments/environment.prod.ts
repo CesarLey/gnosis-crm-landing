44a354150bbf48bfc8b5522bc5f45a0783a8437c
AndrewStevenDJ <andrewstevendiazj@gmail.com>
2026-02-19 10:22:07 -0600

    Correcion del key

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,5 +3,5 @@ export const environment = {
     apiUrl: 'https://gnosis-api.onrender.com', // URL del Backend
     crmUrl: 'https://gnosis-crm-frontend-c395.vercel.app', // URL del CRM Prod
     paypalClientId: 'AcWuv4-mo0o9ST-fQyykx-UawZyI3HVm5rs9PidjIlYcFER61a5Lgz758dHA2A_F6N_-Te9NB4cDQ1mC',
-    recaptchaSiteKey: '6Le_L3EsAAAAAIBo25EtGIICXSAfeXUKv3-pR2eO'
+    recaptchaSiteKey: '6Le_L3EsAAAAAIBo25EtGIlCXSAfeXUKv3-pR2eO'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,5 +3,5 @@ export const environment = {
     apiUrl: 'http://localhost:3000', // URL del Backend Dev
     crmUrl: 'http://localhost:4201', // URL del CRM Dev
     paypalClientId: 'AcWuv4-mo0o9ST-fQyykx-UawZyI3HVm5rs9PidjIlYcFER61a5Lgz758dHA2A_F6N_-Te9NB4cDQ1mC',
-    recaptchaSiteKey: '6Le_L3EsAAAAAIBo25EtGIICXSAfeXUKv3-pR2eO'
+    recaptchaSiteKey: '6Le_L3EsAAAAAIBo25EtGIlCXSAfeXUKv3-pR2eO'
 };
