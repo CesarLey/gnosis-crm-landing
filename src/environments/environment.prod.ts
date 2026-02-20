924df3459f2f18536c0c2b710e14c251393d9a55
AndrewStevenDJ <andrewstevendiazj@gmail.com>
2026-02-20 16:24:14 -0600

    Fix reCAPTCHA site key - correct characters from Google console

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,5 +3,5 @@ export const environment = {
     apiUrl: 'https://gnosis-api.onrender.com', // URL del Backend
     crmUrl: 'https://gnosis-crm-frontend-c395.vercel.app', // URL del CRM Prod
     paypalClientId: 'AcWuv4-mo0o9ST-fQyykx-UawZyI3HVm5rs9PidjIlYcFER61a5Lgz758dHA2A_F6N_-Te9NB4cDQ1mC',
-    recaptchaSiteKey: '6LeGn3IsAAAAANccxY83YlocRKsGLtD9xcLCEz6'
+    recaptchaSiteKey: '6LeGn3IsAAAAAANccxY83YIocRKsGLtD9xcLCEz6'
 };
