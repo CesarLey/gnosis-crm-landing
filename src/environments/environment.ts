0f8bfa3481ac2bbf70b49a3dfaddc8c437c653be
AndrewStevenDJ <andrewstevendiazj@gmail.com>
2026-02-26 10:08:42 -0600

    Update reCAPTCHA keys

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,5 +3,5 @@ export const environment = {
     apiUrl: 'http://localhost:3000', // URL del Backend Dev
     crmUrl: 'http://localhost:4201', // URL del CRM Dev
     paypalClientId: 'AcWuv4-mo0o9ST-fQyykx-UawZyI3HVm5rs9PidjIlYcFER61a5Lgz758dHA2A_F6N_-Te9NB4cDQ1mC',
-    recaptchaSiteKey: '6LeGn3IsAAAAAANccxY83YIocRKsGLtD9xcLCEz6'
+    recaptchaSiteKey: '6LeYwXgsAAAAAJhsBfJ85FZsb8XQP5wFM813oo4g'
 };
